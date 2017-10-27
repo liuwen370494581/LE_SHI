e647184be1af843d1c19b585356862f4548592a2
liuwen <370494581@qq.com>
2017-10-27 09:17:21 +0800

    init project

--- app/src/main/java/star/liuwen/com/le_shi/ChildFragment/MangaFragment.java
+++ app/src/main/java/star/liuwen/com/le_shi/ChildFragment/MangaFragment.java
@@ -51,7 +51,6 @@ public class MangaFragment extends BaseFragment {
 
     @Nullable
     @Override
-
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_manga, container, false);
         init();
