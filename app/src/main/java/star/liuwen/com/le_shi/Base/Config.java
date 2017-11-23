64a33e8e532c9b881cde97ad07eafac08b522475
liuwen <370494581@qq.com>
2017-11-23 10:17:27 +0800

    使用findbug工具 检查代码

--- app/src/main/java/star/liuwen/com/le_shi/Base/Config.java
+++ app/src/main/java/star/liuwen/com/le_shi/Base/Config.java
@@ -4,7 +4,7 @@ package star.liuwen.com.le_shi.Base;
  * Created by liuwen on 2017/10/12.
  */
 public class Config {
-
+    // url  mark
     public static final String TAG = "MainActivity";
     public static final String BAO_FENG_URL = "http://www.baofeng.com/";
     public static final String BAO_FENG_URL_2 = "http://www.baofeng.com";
